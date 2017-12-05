c3bb5b3d15058275152dc039f102ea472ead8bfc
SennaLyn <totatolin@foxmail.com>
2017-12-05 17:06:44 +0800

    react demo

--- src/main.js
+++ src/main.js
@@ -4,15 +4,15 @@ import Header from './components/index.js';
 const ROOT_NODE = document.getElementById('app');
 
 class App extends React.Component{
-    render(){
-        return(
-            <div>
-                <Header/>
-                <div>
-                    others contents
-                </div>
-            </div>
-        )
-    }
+  render(){
+    return(
+      <div>
+        <Header/>
+        <div>
+          others contents
+        </div>
+      </div>
+    )
+  }
 }
-ReactDOM.render(<App/>, ROOT_NODE);  
+ReactDOM.render(<App/>, ROOT_NODE);
