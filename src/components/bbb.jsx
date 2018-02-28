2d72a1be705282e17c4ce1efef29448d8ae89f57
SennaLyn <totatolin@foxmail.com>
2018-02-28 17:43:40 +0800

    practice

--- src/components/bbb.jsx
+++ src/components/bbb.jsx
@@ -2,14 +2,26 @@ import React from 'react';
 class B extends React.Component {
   constructor() {
     super();
+    // this.state = {
+    //   num: 666
+    // }
   }
   render () {
+    var i = 888
+    var Name = React.createClass({
+      render: function () {
+        return (
+          <p>{this.props.name}</p>
+        )
+      }
+    })
     return (
       <div>
       	<p>222</p>
         <p>333</p>
-        <p>444</p>
-        <p>555</p>
+        <p>{i}</p>
+        <p>{10+12}</p>
+        <Name name="linsen" />
       </div>
     )
   }
