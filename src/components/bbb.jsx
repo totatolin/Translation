498b1ed210bb30ca1ece59e0dbb2b247594c3a93
SennaLyn <totatolin@foxmail.com>
2018-03-01 11:22:45 +0800

    practice

--- src/components/bbb.jsx
+++ src/components/bbb.jsx
@@ -2,23 +2,34 @@ import React from 'react';
 class B extends React.Component {
   constructor() {
     super();
-    // this.state = {
-    //   num: 666
-    // }
+    this.state = {
+      num: 999
+    }
   }
   render () {
     var i = 888
     var Name = React.createClass({
+      getInitialState: function () {
+        return {clickNum: 0}
+      },
+      click: function () {
+        this.setState(function(state) {
+          return {clickNum: state.clickNum + 1}
+        })
+      },
       render: function () {
         return (
-          <p>{this.props.name}</p>
+          <div>
+            <p onClick={this.click}>{this.props.name}</p>
+            <p>{this.state.clickNum}</p>
+          </div>
         )
       }
     })
     return (
       <div>
       	<p>222</p>
-        <p>333</p>
+        <p>{this.state.num}</p>
         <p>{i}</p>
         <p>{10+12}</p>
         <Name name="linsen" />
